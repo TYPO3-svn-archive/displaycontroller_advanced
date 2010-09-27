8239c5e627a62c653097856ad1aac863ff39afff
fabien_u <fabien_u@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-27 13:19:54 +0000

    displaycontroller_advanced: released on TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/displaycontroller_advanced@38654 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -1,27 +1,27 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "displaycontrolleradvanced"
+# Extension Manager/Repository config file for ext "displaycontroller_advanced".
 #
-# Auto generated 10-06-2008 13:44
+# Auto generated 27-09-2010 15:10
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Display Controller Advanced',
-	'description' => 'This extension is used to get data from different data models and dispatch it to various views',
+	'title' => 'Advanced Controller  - Tesseract project',
+	'description' => 'This extension manages relations between Tesseract components and produces output in the FE.  More info on http://www.typo3-tesseract.com',
 	'category' => 'plugin',
 	'author' => 'Francois Suter (Cobweb), Fabien Udriot (Ecodev)',
 	'author_email' => 'typo3@cobweb.ch,fabien.udriot@ecodev.ch',
 	'shy' => '',
-	'dependencies' => '',
+	'dependencies' => 'cms,tesseract',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,17 +29,21 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.8.1',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'tesseract' => '0.2.0-0.0.0',
+			'typo3' => '4.3.0-0.0.0',
+			'tesseract' => '0.1.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"8d21";s:10:"README.txt";s:4:"319e";s:38:"class.tx_displaycontrolleradvanced.php";s:4:"7640";s:46:"class.tx_displaycontrolleradvanced_service.php";s:4:"0c8d";s:16:"ext_autoload.php";s:4:"bb6f";s:12:"ext_icon.gif";s:4:"f02f";s:17:"ext_localconf.php";s:4:"94b0";s:14:"ext_tables.php";s:4:"f150";s:14:"ext_tables.sql";s:4:"2aa6";s:16:"ext_typeicon.gif";s:4:"0c33";s:27:"locallang_csh_ttcontent.xml";s:4:"a95d";s:16:"locallang_db.xml";s:4:"bdd8";s:64:"Configuration/TCA/tx_displaycontrolleradvanced_providergroup.php";s:4:"efcf";s:70:"Resources/Public/Images/tx_displaycontrolleradvanced_providergroup.png";s:4:"d3db";s:14:"doc/manual.pdf";s:4:"f467";s:14:"doc/manual.sxw";s:4:"545b";s:46:"pi1/class.tx_displaycontrolleradvanced_pi1.php";s:4:"842a";s:46:"pi2/class.tx_displaycontrolleradvanced_pi2.php";s:4:"b939";}',
 );
 
 ?>
